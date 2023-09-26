9b450da5b4a605ffe146e741fc9f323fd89963ff
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-09-27 00:20:18 +0900

    ./commands/force.jsの追加

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,12 +4,13 @@ const { REST, Routes } = require('discord.js');
 // test.jsのmodule.exportsを呼び出す
 const testFile = require('./commands/test.js');
 const taskFile = require('./commands/task.js');
+const forceFile = require('./commands/force.js');
 
 // 環境変数としてapplicationId, guildId, tokenの3つが必要
 const { applicationId, guildId, token } = require('./config.json');
 
 // 登録コマンドを呼び出してリスト形式で登録
-const commands = [testFile.data.toJSON(), taskFile.data.toJSON()];
+const commands = [testFile.data.toJSON(), taskFile.data.toJSON(), forceFile.data.toJSON()];
 
 // DiscordのAPIには現在最新のversion10を指定
 const rest = new REST({ version: '10' }).setToken(token);
